a55c39f577419df288785ac42503e69479248b2f
František Poboček <pobocekfrantisek@gmail.com>
2023-06-02 12:29:05 +0200

    Binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,18 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+  let lo = 0;
+  let hi = haystack.length;
 
+  do {
+    const m = Math.floor(lo + (hi - lo) / 2);
+
+    if (haystack[m] === needle) return true;
+
+    if (needle > haystack[m]) {
+      lo = m + 1;
+    } else {
+      hi = m;
+    }
+  } while (lo < hi);
+
+  return false;
 }
