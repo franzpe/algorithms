8d683c86e45b44389442911c03ec8bb6289ed340
František Poboček <pobocekfrantisek@gmail.com>
2023-06-02 09:03:21 +0200

    Linear search list

--- src/day1/LinearSearchList.ts
+++ src/day1/LinearSearchList.ts
@@ -1,3 +1,12 @@
 export default function linear_search(haystack: number[], needle: number): boolean {
+  let result = false;
 
+  for (let i = 0; i < haystack.length; i++) {
+    if (haystack[i] === needle) {
+      result = true;
+      break;
+    }
+  }
+
+  return result;
 }
