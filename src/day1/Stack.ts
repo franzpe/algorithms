89cd33267d9c70f87f5bcf1d4b127e949ce239d5
František Poboček <pobocekfrantisek@gmail.com>
2023-06-03 19:17:13 +0200

    Stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,47 @@
+type Node<T> = {
+  value: T;
+  previous?: Node<T>;
+};
+
 export default class Stack<T> {
-    public length: number;
+  public length: number;
+  private head?: Node<T>;
 
-    
+  constructor() {
+    this.head = undefined;
+    this.length = 0;
+  }
 
-    constructor() {
+  push(item: T): void {
+    const node = { value: item } as Node<T>;
+
+    this.length++;
+
+    if (!this.head) {
+      this.head = node;
+      return;
     }
 
-    push(item: T): void {
+    node.previous = this.head;
+    this.head = node;
+  }
 
-}
-    pop(): T | undefined {
+  pop(): T | undefined {
+    this.length = Math.max(0, this.length - 1);
 
-}
-    peek(): T | undefined {
+    if (this.length === 0) {
+      const head = this.head;
+      this.head = undefined;
+      return head?.value;
+    }
 
-}
+    const head = this.head as Node<T>;
+    this.head = head.previous;
+
+    return head.value;
+  }
+
+  peek(): T | undefined {
+    return this.head?.value;
+  }
 }
